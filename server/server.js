8b9b5841e67666890023642e1c9c02945cab33b4
SavindiMuthukumarane <savindimuthukumarane@gmail.com>
2026-08-16 15:39:03 +0530

    M2-Initialize Express Server

---
+++ server/server.js
@@ -0,0 +1,6 @@
+import 'dotenv/config';
+import app from './app.js';
+
+const port = Number(process.env.PORT) || 5000;
+
+app.listen(port, '0.0.0.0', () => console.log(`CollabBoard M2 API running on port ${port}`));
